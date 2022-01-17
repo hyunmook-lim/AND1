114f381a8b504a91ad2c076b0762c2f7eb02b37e
hyunmook-lim <53961996+hyunmook-lim@users.noreply.github.com>
2022-01-17 21:37:50 +0900

    commit

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,10 @@
 import Login from "./Login";
 import EmailLogin from "./EmailLogin";
+<<<<<<< Updated upstream
 import Singup from "./Signup";
+=======
+import Singin from "./SignIn";
+>>>>>>> Stashed changes
 import FriendList from "./FriendList";
 import MakingGroup from "./MakingGroup";
 import TeamSearch from "./TeamSearch";
