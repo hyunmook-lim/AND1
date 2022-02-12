cd7e1b3833b24d86a812c2ef5f6416435665ed0f
hyunmook-lim <hmlim0320@naver.com>
2022-02-12 20:01:26 +0900

    background of MakingGroup.js
    sketch

--- src/screens/MakingGroup.js
+++ src/screens/MakingGroup.js
@@ -8,16 +8,50 @@ const Container = styled.SafeAreaView`
   background-color: ${({ theme }) => theme.background};
 `;
 
+const TeamSettingCon = styled.SafeAreaView`
+  flex: 2;
+  width : 90%;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  margin : 10px;
+`
+
+const MembersCon = styled.SafeAreaView`
+  flex : 1;
+  width : 90%;
+  justify-content: center;
+  align-items: center;
+  background-color: blue;
+  margin: 10px;
+  `
+
 const Text = styled.Text`
   color: ${({ theme }) => theme.normalText}
   font-size: 40px;
   font-weight: bold
 `;
+const TeamSearchButtion = styled.TouchableOpacity`
+  flex: 0.3;
+  width : 90%;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  margin: 10px; 
+`
 
 export default function MakingGroup() {
   return (
     <Container>
-      <Text>Making Group Page</Text>
+      <TeamSettingCon>
+        <Text>Team setting</Text>
+      </TeamSettingCon>
+      <MembersCon>
+        <Text>Members</Text>
+      </MembersCon>
+      <TeamSearchButtion>
+        <Text>TeamSerachButton</Text>
+      </TeamSearchButtion>
     </Container>
   );
 }
